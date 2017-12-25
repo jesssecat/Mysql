bfabf7c0c217bbfafb562717bbdaedb02b2e3759
jesssecat <813070871@qq.com>
2017-12-25 16:01:25 +0800

    Update mysql.func.php

--- mysql.func.php
+++ mysql.func.php
@@ -1,11 +1,12 @@
 <?php 
 	//数据库链接信息封装，保存
 	function connects($config){
-		$link=mysql_connect($config['host'],$config['username'],$config['password']) or die("数据库链接失败,检测数据库链接信息");
+		$link=mysql_connect($config['host'],$config['username'],$config['password'],$config['dbName']) or die("数据库链接失败,检测数据库链接信息");
 		mysql_set_charset($config['charset']);
-		mysql_select_db($config['dbname']);
-		//return $link;
+		mysql_select_db($config['dbName']);
+		//mysql_select_db("test");//打开指定数据库
+		return $link;
 	}
-	$config = array("host" => "localhost","username"=>"root","dbName"=>"test","charset"=>"utf8","password"=>"root");
+	$config = array("host" => "localhost","username"=>"root","dbName"=>"hao","charset"=>"utf8","password"=>"root");
 	connects($config);
  ?>
